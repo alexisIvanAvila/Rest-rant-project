efcef0e97e46c9d0592cf00dd299b400107e3dd1
alexis avila <evan.avila1@gmail.com>
2022-11-04 19:44:00 -0700

    adding defualt view for layout

---
+++ views/default.jsx
@@ -0,0 +1,17 @@
+const React = require('react')
+
+function Def (html) {
+    return (
+        <html>
+            <head>
+                <title>Title</title>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </html>
+    )
+  }
+  
+
+module.exports = Def
