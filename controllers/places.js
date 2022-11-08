9f9fbe04d3f99d92ed546589afc65df5dac068bb
alexis avila <evan.avila1@gmail.com>
2022-11-08 11:55:04 -0800

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,22 @@
 const router = require('express').Router()
+const places =require('../models/places.js')
+
+router.post('/', (req, res) => {
+    
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  })
+  
 
 //GET / Places
 router.get('/', (req, res) => {
